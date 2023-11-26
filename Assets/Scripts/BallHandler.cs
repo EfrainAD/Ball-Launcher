8614d94519452ddc8e8280c0d7e5fa34cf30978e
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-11-26 01:56:47 -0500

    Launching the ball with 2 Challenges along the way

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,6 +8,9 @@ public class BallHandler : MonoBehaviour
 {
     private Camera mainCamera;
     [SerializeField] private Rigidbody2D ballRigidbody;
+    [SerializeField] private SpringJoint2D ballSpringJoint;
+    [SerializeField] private float delayDuration;
+    private bool isDragging;
 
 
     // Start is called before the first frame update
@@ -18,17 +22,37 @@ public class BallHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (ballRigidbody == null) {return;}
+
         if (Touchscreen.current.primaryTouch.press.isPressed)
         {
             Vector2 touchLoc = Touchscreen.current.primaryTouch.position.ReadValue();
             
             Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchLoc);
 
-            ballRigidbody.isKinematic = true;
             ballRigidbody.position = worldPosition;
+            isDragging = true;
+            ballRigidbody.isKinematic = true;
         }
         else {
-            ballRigidbody.isKinematic = false;
+            if (isDragging)
+            {
+                isDragging = false;
+                
+                LaunchBall();
+            }
         }
+    }
+
+    private void LaunchBall() 
+    {
+        ballRigidbody.isKinematic = false;
+        ballRigidbody = null;
+        Invoke(nameof(DetatchBall), delayDuration);
+    }
+    private void DetatchBall() 
+    {
+        ballSpringJoint.enabled = false;
+        ballSpringJoint = null;
     }
 }
